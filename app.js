ece6a58510d713d6f94e3f08fa35ff279e0281d3
aboublef <kristopherab@gmail.com>
2023-02-15 21:35:40 +0000

    .

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const config = require("./config");
 const express = require("express");
-const photoRouter = require("./routes/photoRoutes");
+const mongoose = require("mongoose");
 
 const app = express();
 
+app
+    .use(express.json())
+    .use("/api/photos", require("./routes/photoRoutes"))
+    .use("/api/users", require("./routes/userRoutes"))
+    .listen(config.port, main);
 
-app.use(express.json());
-
-app.use("/api/photos", photoRouter);
-
-app.listen(config.port, () => {
+async function main() {
     console.log(`Listening on port ${config.port}`);
-});
+    try {
+        await mongoose.connect(config.mongo);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        // close the server
+        console.log("Error connecting to MongoDB", error);
+        process.exit(1);
+    }
+}
