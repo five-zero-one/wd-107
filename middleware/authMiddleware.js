822e142e1ad2ef0403117aae145c5e7a600cdf86
Rahim <34522491+adoublef@users.noreply.github.com>
2023-02-16 00:50:00 +0000

    Submittion (#7)
    * .
    
    * update controllers and add middleware to respective files
    
    * .
    
    * sample added
    
    ---------
    
    Co-authored-by: five-zero-one <110472027+five-zero-one@users.noreply.github.com>

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 const { expressjwt } = require("express-jwt");
 const config = require("../config");
 
-
 exports.validateToken = expressjwt({
     secret: config.jwtSecret,
     algorithms: ["HS256"],
